9c70c8b2a1daa34ca953632960a0d2ace1df55d1
Marco Navarro <7016419+marconn@users.noreply.github.com>
2023-01-17 15:06:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.json(req.body);
+app.post("/", (req, res) => res.json(req.body));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
