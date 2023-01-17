c1309cbeeaaf6cd7999e1c6800ae967f727f6f41
Marco Navarro <7016419+marconn@users.noreply.github.com>
2023-01-17 16:00:54 -0600

    Adding timeout logic

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.post("/", (req, res) => res.json(req.body));
+app.use(express.json())
+
+app.post("/", async function(req, res) {
+    let number = getRandom(1, 11);
+    if (number <= 6) {
+        number = getRandom(1, 4);
+        await sleep(number * 1000);
+        return res.json(req.body);
+    }
+    return res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+/**
+ * Returns a random number between min (inclusive) and max (exclusive)
+ */
+function getRandom(min, max) {
+    return Math.floor(
+        Math.random() * (max - min) + min
+    )
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
